ff954d0b45568be4ca0a3c0f7fc362793315f2c5
Leonardo <leonardo_vieira199@hotmail.com>
2026-07-01 07:23:27 -0300

    sql script

--- backend/src/main/java/com/lsouzadev/aula/entity/Category.java
+++ backend/src/main/java/com/lsouzadev/aula/entity/Category.java
@@ -3,7 +3,9 @@ package com.lsouzadev.aula.entity;
 import jakarta.persistence.*;
 
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_category")
@@ -19,6 +21,9 @@ public class Category {
 
     @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
     private Instant updatedAt;
+
+    @ManyToMany(mappedBy = "categories")
+    private Set<Product> products = new HashSet<>();
 
     public Category() {
     }
